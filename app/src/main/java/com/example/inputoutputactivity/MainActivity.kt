4aab4dd3942e8b17fb29892cd7116bb05aab003a
otha-31 <othavatela2@gmail.com>
2026-03-10 16:50:18 +0200

    Added A Toast Button To input and output project

---
+++ app/src/main/java/com/example/inputoutputactivity/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.inputoutputactivity
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        //get the button using the id we set on the user interface
+        val clickMeButton = findViewById<Button>(R.id.clickbutton)
+        //add code to the button that happens when code is clicked
+        clickMeButton?.setOnClickListener{
+            Toast.makeText(this@MainActivity,
+                "Button clicked",Toast.LENGTH_LONG).show()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
