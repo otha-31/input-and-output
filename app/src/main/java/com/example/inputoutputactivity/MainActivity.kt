417cbe1cd46487d518c4fd5bdafbe24dd73ea81d
otha-31 <othavatela2@gmail.com>
2026-03-12 09:24:21 +0200

    added Id's for the click project

--- app/src/main/java/com/example/inputoutputactivity/MainActivity.kt
+++ app/src/main/java/com/example/inputoutputactivity/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.inputoutputactivity
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,12 +15,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         //get the button using the id we set on the user interface
         val clickMeButton = findViewById<Button>(R.id.clickbutton)
+        val welcomeTxt =findViewById<TextView>(R.id.displayTxt)
+        val nameTxtField = findViewById<EditText>(R.id.nameTxt)
+
         //add code to the button that happens when code is clicked
         clickMeButton?.setOnClickListener{
+            //
+
             Toast.makeText(this@MainActivity,
                 "Button clicked",Toast.LENGTH_LONG).show()
+            //
+
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
